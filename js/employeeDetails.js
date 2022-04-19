f6fbb42c58258beed1f19c00c9da798b5cd9796f
Mohamed Mujahith Ali <49042755+mujahithali@users.noreply.github.com>
2022-04-19 16:09:27 +0530

    Add files via upload

---
+++ js/employeeDetails.js
@@ -0,0 +1,130 @@
+let emplyJsonObj =
+    [
+        {
+            "firstName": "employee 1001 FN",
+            "lastName": "employee 1001 LN",
+            "employeeId": 1001,
+            "myTeam": [
+                {
+                    "firstName": "employee 1002 FN",
+                    "lastName": "employee 1002 LN",
+                    "employeeId": 1002,
+                    "myTeam": [
+                        {
+                            "firstName": "employee 1003 FN",
+                            "lastName": "employee 1003 LN",
+                            "employeeId": 1003,
+                            "myTeam": [
+                                {
+                                    "firstName": "employee 1004 FN",
+                                    "lastName": "employee 1004 LN",
+                                    "employeeId": 1004,
+                                    "myTeam": [
+                                        {
+                                            "firstName": "employee 1005 FN",
+                                            "lastName": "employee 1005 LN",
+                                            "employeeId": 1005,
+                                            "myTeam": [{
+                                            }]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "firstName": "employee 1006 FN",
+            "lastName": "employee 1006 LN",
+            "employeeId": 1006,
+            "myTeam": [
+                {
+                    "firstName": "employee 1007 FN",
+                    "lastName": "employee 1007 LN",
+                    "employeeId": 1007,
+                    "myTeam": [
+                        {
+                            "firstName": "employee 1008 FN",
+                            "lastName": "employee 1008 LN",
+                            "employeeId": 1008,
+                            "myTeam": [
+                                {
+                                    "firstName": "employee 1009 FN",
+                                    "lastName": "employee 1009 LN",
+                                    "employeeId": 1009,
+                                    "myTeam": [
+                                        {
+                                            "firstName": "employee 1010 FN",
+                                            "lastName": "employee 1010 LN",
+                                            "employeeId": 1010,
+                                            "myTeam": [{
+                                            }]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "firstName": "employee 1011 FN",
+                    "lastName": "employee 1011 LN",
+                    "employeeId": 1011,
+                    "myTeam": [{
+                        "firstName": "employee 1012 FN",
+                        "lastName": "employee 1012 LN",
+                        "employeeId": 1012,
+                        "myTeam": [{
+                            "firstName": "employee 1013 FN",
+                            "lastName": "employee 1013 LN",
+                            "employeeId": 1013,
+                            "myTeam": [{
+                                "firstName": "employee 1014 FN",
+                                "lastName": "employee 1014 LN",
+                                "employeeId": 1014,
+                                "myTeam": [{
+                                    "firstName": "employee 1015 FN",
+                                    "lastName": "employee 1015 LN",
+                                    "employeeId": 1015,
+                                    "myTeam": [{
+                                    }]
+                                }]
+                            }]
+                        }]
+                    }]
+                }
+            ]
+        },
+        {
+            "firstName": "employee 1016 FN",
+            "lastName": "employee 1016 LN",
+            "employeeId": 1016,
+            "myTeam": 
+            [
+                {
+                    "firstName": "employee 1017 FN",
+                    "lastName": "employee 1017 LN",
+                    "employeeId": 1017,
+                    "myTeam": [{
+                        "firstName": "employee 1018 FN",
+                        "lastName": "employee 1018 LN",
+                        "employeeId": 1018,
+                        "myTeam": [{
+                            "firstName": "employee 1019 FN",
+                            "lastName": "employee 1019 LN",
+                            "employeeId": 1019,
+                            "myTeam": [{
+                                "firstName": "employee 1020 FN",
+                                "lastName": "employee 1020 LN",
+                                "employeeId": 1020,
+                                "myTeam": [{
+                                }]
+                            }]
+                        }]
+                    }]
+                }
+            ]
+        }
+    ];
